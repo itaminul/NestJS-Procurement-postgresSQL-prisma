4c863630aef9ee1f4e4e32508b2ac31ad725fc05
itaminul <itaminul@gmail.com>
2024-02-01 21:13:05 +0600

    vendor modify

--- src/setup/vendor-setup/vendor-setup.controller.ts
+++ src/setup/vendor-setup/vendor-setup.controller.ts
@@ -3,16 +3,4 @@ import { VendorSetupService } from './vendor-setup.service';
 
 @Controller('vendorsetup')
 export class VendorSetupController {
-    constructor(private readonly vendorService: VendorSetupService ) {}
-
-    @Get()
-    getVendoer() {
-        try {
-            const result = this.vendorService.getVendor();
-            return result;
-        } catch (error) {
-            console.log("error", error);
-        }
-    }
-    
 }
