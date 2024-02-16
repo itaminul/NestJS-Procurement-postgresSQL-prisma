5e8bc6e0350e83af370d5960b80c92131ce778f5
itaminul <itaminul@gmail.com>
2024-02-16 21:37:32 +0600

    item serviec

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,9 @@ import { ConfigService } from '@nestjs/config';
 import { VendorSetupCountryModule } from './setup/vendor-country/vendor.setup.country.module';
 import { VendorSetupCountryController } from './setup/vendor-country/vendor.setup.country.controller';
 import { VendorSetupCountryService } from './setup/vendor-country/vendor.setup.country.service';
+import { ItemSetupModule } from './procurements/item-setup/item-setup.module';
 @Module({
-  imports: [VendorSetupModule, VendorSetupCountryModule],
+  imports: [VendorSetupModule, VendorSetupCountryModule, ItemSetupModule],
   controllers: [VendorSetupController, VendorSetupCountryController],
   providers: [VendorSetupService, PrismaService, ConfigService, VendorSetupCountryService],
 })
