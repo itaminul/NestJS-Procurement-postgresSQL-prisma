7ba6edfc5f0fc2b31e3d3bf5656febdb9c777351
itaminul <itaminul@gmail.com>
2024-02-19 21:37:22 +0600

    modify item setup controller

--- src/procurements/item-setup/item-setup.controller.ts
+++ src/procurements/item-setup/item-setup.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { ItemSetupService } from './item-setup.service';
+import { CreateItemDto } from './dto/create.item.dto';
+import { UpdateItemDto } from './dto/update.item.dto';
 
 @Controller('item-setup')
-export class ItemSetupController {}
+export class ItemSetupController {
+  constructor(public readonly itemSetupService: ItemSetupService) {};
+  @Get()
+  async getAll () {
+    try {
+      const results = await this.itemSetupService.getAll();
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+
+  @Post()
+  async create(@Body() createItemDto: CreateItemDto) {
+    try {
+      const results = await this.itemSetupService.create(createItemDto);
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+
+  @Patch('/:id')
+  async update(@Param('id') id: number, updateItemDto:UpdateItemDto) {
+    try {
+      const results = await this.itemSetupService.update(id, updateItemDto);
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+}
