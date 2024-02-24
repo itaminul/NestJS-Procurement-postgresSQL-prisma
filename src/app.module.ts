3a4a006a7b4d8e3a484b5383eb95756ea109c551
itaminul <itaminul@gmail.com>
2024-02-24 23:45:12 +0600

    file upload service

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,29 @@ import { VendorSetupCountryController } from './setup/vendor-country/vendor.setu
 import { VendorSetupCountryService } from './setup/vendor-country/vendor.setup.country.service';
 import { ItemSetupModule } from './procurements/item-setup/item-setup.module';
 import { PrismaModule } from './db/prisma.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { UploadService } from './file-upload/upload.service';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MulterModule.register({
+      dest: './uploads',
+    }),
+
     PrismaModule,
-    VendorSetupModule, VendorSetupCountryModule, ItemSetupModule
+    VendorSetupModule,
+    VendorSetupCountryModule,
+    ItemSetupModule,
   ],
   controllers: [VendorSetupController, VendorSetupCountryController],
-  providers: [VendorSetupService, PrismaService, ConfigService, VendorSetupCountryService],
+  providers: [
+    VendorSetupService,
+    PrismaService,
+    ConfigService,
+    VendorSetupCountryService,
+    UploadService
+  ],
 })
 export class AppModule {}
